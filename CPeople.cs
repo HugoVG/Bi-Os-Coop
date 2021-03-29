4f850fdbed9da754dcbb64e785ea157a8b4703fa
BMooldijk <bjornmooldijk@gmail.com>
2021-03-29 11:47:07 +0200

    Makes the change password method work

--- CPeople.cs
+++ CPeople.cs
@@ -4,26 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.Json;
+using System.IO;
 
 namespace Bi_Os_Coop
 {
     class CPeople
     {
-        public static void Poggers()
+        public static void NewUser()
         {
-            People people = new People();
-            people.addPersonByFunction(1, "Henk", "henk@henkino.com", "H3nko", "23/3/2021");
+            string json = Json.ReadJson("Accounts");
+            People jsonPeople = JsonSerializer.Deserialize<People>(json);
 
-            people.writePeople();
-            Console.WriteLine(people.ToJson());
-            People newPeople = people.FromJson(people.ToJson());
-
-            newPeople.writePeople();
-            people = people.FromJson(people.ToJson());
-            //Person Harrie = people.peopleList.Single(henk => henk.email == "mijnfakeEmail@test.com" && henk.password == "H3nk123");
-            //Harrie.ChangePassword();
-
+            jsonPeople.addPersonByFunction(3, "Bjorn", "json@bjorn.com", "jsonBjorn", "30", "06123456789");
+            json = JsonSerializer.Serialize(jsonPeople);
+            Json.WriteJson("Accounts", json);
+            //Console.WriteLine(jsonPeople);
         }
+
         /// <summary>
         /// Person class
         /// Fields:
@@ -36,31 +33,178 @@ namespace Bi_Os_Coop
             public string email { get; set; }
             public string password { get; set; }
             public string age { get; set; }
+            public string phonenumber { get; set; }
             //If you gonna edit this EDIT ALL
-            public void setPerson(int id, string name, string email, string password, string age)
+            public void setPerson(int id, string name, string email, string password, string age, string phonenumber)
             {
                 this.id = id;
                 this.name = name;
                 this.email = email;
                 this.password = password;
                 this.age = age;
+                this.phonenumber = phonenumber;
             }
 
             // general methods
             public void Login()
             {
-
+                Console.Clear();
+                //Person Ingelogd = loginscherm.login();
+                //return Ingelogd;
             }
 
-            public void Logout()
+            public Person Logout()
             {
-
+                Person Ingelogd = new Person();
+                return Ingelogd;
             }
 
-            public void ChangePassword()
+            public void ChangePassword(Person ingelogdepersoon)
             {
+                if (ingelogdepersoon.id != 0) // person is logged in
+                {
+                    Console.WriteLine("Vul uw emailadres in:");
+                    string currentEmail = Console.ReadLine();
 
+                    Console.WriteLine("Vul uw huidige wachtwoord in:");
+                    string currentPassword = Console.ReadLine();
+
+                    if (MailWachtwoordCheck(currentEmail, currentPassword))
+                    {
+                        SetNewPassword();
+                    }
+                    else
+                    {
+                        int amountOfPasswordEntries = 3;
+                        while (amountOfPasswordEntries > 0)
+                        {
+                            Console.WriteLine($"Wachtwoord of email onjuist. U heeft nog {amountOfPasswordEntries} pogingen.");
+                            Console.WriteLine("Vul nogmaals uw emailadres in:");
+                            currentEmail = Console.ReadLine();
+
+                            Console.WriteLine("Vul nogmaals uw wachtwoord in:");
+                            currentPassword = Console.ReadLine();
+
+                            if (currentPassword == password && currentEmail == email)
+                            {
+                                SetNewPassword();
+                                break;
+                            }
+                            else
+                            {
+                                amountOfPasswordEntries--;
+                                if (amountOfPasswordEntries == 0)
+                                {
+                                    Console.WriteLine("0 pogingen. Probeer het later nog eens.");
+                                }
+                            }
+                        }
+                    }
+                }
+
+                else if (ingelogdepersoon.id == 0) // person is not logged in
+                {
+                    Console.WriteLine("Vul uw emailadres in:");
+                    string currentEmail = Console.ReadLine();
+
+                    Console.WriteLine("Vul uw geboortedatum in:");
+                    string currentAge = Console.ReadLine();
+
+                    if (MailLeeftijdCheck(currentEmail, currentAge))
+                    {
+                        SetNewPassword();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, dit account bestaat niet.");
+                        Console.WriteLine("Wilt u een nieuw account aanmaken? (Antwoord met ja of nee)");
+                        string antwoordAanmakenNieuwAccount = Console.ReadLine();
+                        if (antwoordAanmakenNieuwAccount.ToLower() == "ja")
+                        {
+
+                        }
+                    }
+                }
+
+                else
+                {
+                    throw new NotImplementedException();
+                }
             }
+
+            public void SetNewPassword()
+            {
+                Console.Clear();
+                Console.WriteLine("Vul nu uw nieuwe wachtwoord in:");
+                string newPassword = Console.ReadLine();
+                Console.WriteLine("Vul nogmaals uw nieuwe wachtwoord in:");
+                string tempNewPassword = Console.ReadLine();
+
+                if (tempNewPassword == newPassword)
+                {
+                    string json = Json.ReadJson("Accounts");
+                    People jsonPeople = JsonSerializer.Deserialize<People>(json);
+                    try
+                    {
+                        Person tempPerson = jsonPeople.peopleList.Single(person => person.email == email && person.password == password);
+                        password = newPassword;
+                        tempPerson.password = newPassword;
+
+                        json = JsonSerializer.Serialize(jsonPeople);
+                        Json.WriteJson("Accounts", json);
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("U heeft uw wachtwoord succesvol gewijzigd.");
+                        System.Threading.Thread.Sleep(2000);
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Clear();
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        Console.WriteLine("Dit account bestaat niet.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("U had niet tweemaal hetzelfde nieuwe wachtwoord ingevoerd.");
+                    System.Threading.Thread.Sleep(2000);
+                    SetNewPassword();
+                }
+            }
+
+
+            public static bool MailLeeftijdCheck(string username, string age)
+            {
+                string account = Json.ReadJson("Accounts");
+                People accounts = new People();
+                accounts = accounts.FromJson(account);
+                try
+                {
+                    Person persoon = accounts.peopleList.Single(person => person.email == username && person.age == age);
+                    return true;
+                }
+                catch (InvalidOperationException)
+                {
+                    return false;
+                }
+            }
+
+            public static bool MailWachtwoordCheck(string username, string password)
+            {
+                string account = Json.ReadJson("Accounts");
+                People accounts = new People();
+                accounts = accounts.FromJson(account);
+                try
+                {
+                    Person persoon = accounts.peopleList.Single(person => person.email == username && person.password == password);
+                    return true;
+                }
+                catch (InvalidOperationException)
+                {
+                    return false;
+                }
+            }
+
 
             public void ViewMovies()
             {
@@ -113,7 +257,7 @@ namespace Bi_Os_Coop
         public class People
         {
             public List<Person> peopleList { get; set; }
-            public List<Admin>  adminList { get; set; }
+            public List<Admin> adminList { get; set; }
             public List<Employee> employeeList { get; set; }
 
             /// <summary>
@@ -174,10 +318,10 @@ namespace Bi_Os_Coop
             /// <param name="email"></param>
             /// <param name="password"></param>
             /// <param name="age"></param>
-            public void addPersonByFunction(int id, string name, string email, string password, string age)
+            public void addPersonByFunction(int id, string name, string email, string password, string age, string phonenumber)
             {
                 Person temp = new Person();
-                temp.setPerson(id, name, email, password, age);
+                temp.setPerson(id, name, email, password, age, phonenumber);
                 AddPerson(temp);
             }
 
@@ -191,6 +335,7 @@ namespace Bi_Os_Coop
                     Console.Write($"email:{person.email} \t");
                     Console.Write($"password:{person.password} \t");
                     Console.Write($"age:{person.age} \n");
+                    Console.Write($"phonenumber:{person.phonenumber} \n");
                 }
             }
 
