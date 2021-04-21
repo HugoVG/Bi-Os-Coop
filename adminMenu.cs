83d888ee87a78caf798676f79c821f58cba4824e
Hajar Akkouh <1008536@hr.nl>
2021-04-21 02:15:30 +0200

    methods admin geïmplementeerd

--- adminMenu.cs
+++ adminMenu.cs
@@ -7,32 +7,25 @@ using System.Text.Json;
 
 namespace Bi_Os_Coop
 {
-    class admin
-    {
-        public int zalen { get; set; }
-        public string[] Film { get; set; }
-    }
-
     public class adminMenu
     {
         public static void AM()
         {
-            string path = "../../Json/admin.json";
-            string tekstUitJson = System.IO.File.ReadAllText(path);
-            admin objectTest = new admin();
-            objectTest = JsonSerializer.Deserialize<admin>(tekstUitJson);
+            CPeople.Admin admin = new CPeople.Admin();
+            CPeople.Person adminAccount = new CPeople.Person();
+            adminAccount.setPerson(3, "admin", "admin123@gmail.com", "admin123", "42", "0681234567");
+            //string path = "../../Json/admin.json";
+            //string tekstUitJson = System.IO.File.ReadAllText(path);
+            //admin objectTest = new admin();
+            //objectTest = JsonSerializer.Deserialize<admin>(tekstUitJson);
 
-            JsonSerializerOptions options = new JsonSerializerOptions() { WriteIndented = true };
-            tekstUitJson = JsonSerializer.Serialize(objectTest, options);
-            System.IO.File.WriteAllText(path, tekstUitJson);
-
-            
-            // deze twee zijn er alleen zodat de rest niet 20483 errors geeft 
-            Tuple<string, string, string>[] filmsArray = new Tuple<string, string, string>[15];
-            int aantalFilms = 0;
+            //JsonSerializerOptions options = new JsonSerializerOptions() { WriteIndented = true };
+            //tekstUitJson = JsonSerializer.Serialize(objectTest, options);
+            //System.IO.File.WriteAllText(path, tekstUitJson);     
 
             //while loop die hoofdPagina loopt tot je "0" in tikt
-            while (true)
+            bool inDitMenu = true;
+            while (inDitMenu)
             {
                 string keuze = hoofdPagina();
                 if (keuze == "0")
@@ -41,86 +34,55 @@ namespace Bi_Os_Coop
                 }
                 else if (keuze == "1")
                 {
+                    inDitMenu = false;
                     Console.Clear();
-                    Console.WriteLine("Hoeveel zalen wilt u?");
-                    objectTest.zalen = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine(objectTest.zalen);
+                    MainMenu.MainMenuShow();
                 }
                 else if (keuze == "2")
                 {
-                    filmsArray[aantalFilms] = FilmsToevoegen();
-                    aantalFilms += 1;
+                    admin.AddMovies();
                 }
-                
+
                 else if (keuze == "3")
                 {
-                    Console.WriteLine(FilmsAanpassen());
+                    admin.UpdateMovies();
                 }
+                else if (keuze == "4")
+                {
+                    admin.DeleteMovies();
+                }
+                else if (keuze == "5")
+                {
+                    admin.AddCinemaHall();
+                }
+                //else if (keuze == "6")
+                //{
+                //    admin.DeleteCinemaHall();
+                //}
                 else
                 {
                     hoofdPagina();
                 }
-                /* dit werkt nog niet helemaal met de while loop dus heb het ff in een comment gezet zodat ik het kon commiten 
-                if (keuze == "4")
-                {
-                    Console.Clear();
-                    Console.WriteLine(filmsArray);
-                }
-                else if (keuze == "5")
-                {
-                    Console.Clear();
-                    Console.WriteLine(zalen);
-                }
-                */
             }
         }
 
         public static string hoofdPagina()
         {
             Console.Clear();
+            MainMenu.Logo();
             Console.WriteLine("Admin Menu\n");
-            Console.WriteLine("Choose an option:");
-            Console.WriteLine("1) Aantal zalen selecteren");
+            Console.WriteLine("Maak een keuze: ");
+            Console.WriteLine("1) Naar Main Menu");
             Console.WriteLine("2) Film toevoegen");
             Console.WriteLine("3) Film aanpassen");
-            //Console.WriteLine("4) Alle Films");
-            //Console.WriteLine("5) Aantal Zalen");
+            Console.WriteLine("4) Film verwijderen");
+            Console.WriteLine("5) Zaal toevoegen");
+            //Console.WriteLine("6) Zaal verwijderen");
+            //Console.WriteLine("7) Admin Toevoegen");
             Console.WriteLine("Of type '0' om te stoppen");
             Console.Write("\nKies een pagina: ");
             string keuze = Console.ReadLine();
             return keuze;
-        }
-
-        //public static string AantalZalen()
-        //{
-        //    Console.Clear();
-        //    Console.WriteLine("Hoeveel zalen wilt u?");
-        //    string zalen = Console.ReadLine();
-        //    return zalen;
-        //}
-
-        public static Tuple<string, string, string> FilmsToevoegen()
-        {
-            Console.Clear();
-            Console.WriteLine("Welke film wilt u toevoegen?");
-            string film = Console.ReadLine();
-            Console.WriteLine("In welk jaar kwam de film uit?");
-            string jaar = Console.ReadLine();
-            Console.WriteLine("Tot welke genre behoort deze film?");
-            string genre = Console.ReadLine();
-            return Tuple.Create(film, jaar, genre);
-        }
-
-        public static Tuple<string, string, string> FilmsAanpassen()
-        {
-            Console.Clear();
-            Console.WriteLine("Welke film wilt u aanpassen?");
-            string film = Console.ReadLine();
-            Console.WriteLine("In welk jaar kwam de film uit?");
-            string jaar = Console.ReadLine();
-            Console.WriteLine("Tot welke genre behoort deze film?");
-            string genre = Console.ReadLine();
-            return Tuple.Create(film, jaar, genre);
         }
     }
 }
